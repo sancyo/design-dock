154171ad3571d1f813dba63f925e29806ffd336f
sancyo <b1845958@planet.kanazawa-it.ac.jp>
2020-05-16 14:11:35 +0900

    add:ヘッダーを追加。normarize.cssを追加

---
+++ layouts/ThePageHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container header-container">
+    <div class="inner header-inner">
+      <h1>
+        <nuxt-link to="/" class="site-logo">
+          <img class="logo" src="@/static/site-logo@2x.png" alt="DesignDock" />
+        </nuxt-link>
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.header-container {
+  background-color: #fff;
+}
+.header-inner {
+  background-color: #fff;
+}
+.logo {
+  padding: 0.8em 0;
+  width: 170px;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,40 @@
 <template>
   <div>
+    <the-page-header />
     <nuxt />
   </div>
 </template>
+
+<script>
+import ThePageHeader from '@/layouts/ThePageHeader'
+export default {
+  components: {
+    ThePageHeader
+  }
+}
+</script>
 
 <style>
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+  font-size: 62.5%;
+  background-color: #dce1eb;
 }
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
+h1,
+h2,
+h3,
+p {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+.container {
+  margin: 0 auto;
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.inner {
+  max-width: 1024px;
+  margin: 0 auto;
 }
 </style>
